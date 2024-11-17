cfbea30cb16cceb38785007cba0e15c90beaf890
nioomeee <nioomeee@gmail.com>
2024-11-18 00:23:13 +0530

    3.10

---
+++ OOPS/assignment 3/10.cpp
@@ -0,0 +1,42 @@
+// Write a C++ program to enter employee details like name & Salary using Array of Object
+#include<iostream>
+#include <string>
+using namespace std;
+
+class Employee{
+private: 
+    int age;
+    string name;
+    float salary;
+public:
+    void getData(){
+        cout << "\n ENter yoru name: ";
+        cin >> name;
+        cout << "\n Enter your age: ";
+        cin >> age;
+        cout << "\n Enter your salary: ";
+        cin >> salary;
+    }
+    void Show(){
+        cout << "\n age = "<<age<<endl;
+        cout << "\n name = "<<name<<endl;
+        cout << "\n salary = "<<salary<<endl;
+    }
+};
+
+int main(){
+    
+    Employee emp[3];
+    string name;
+    int age;
+    float salary;
+
+    for(int i = 0; i < 3; i ++){
+        cout<<"\n For employee"<<i+1<<": "<<endl;
+        emp[i].getData();
+        emp[i].Show();
+    }
+    
+    cout<<endl;
+    return 0;
+}
